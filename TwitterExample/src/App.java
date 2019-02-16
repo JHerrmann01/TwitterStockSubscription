eee48e124bf35b0a5c6e60c4885f5345f3ca92d5
unknown <jeremy.herrmann@stonybrook.edu>
2019-02-15 19:00:00 -0500

    With twitter!

---
+++ TwitterExample/src/App.java
@@ -0,0 +1,15 @@
+import twitter4j.Status;
+import twitter4j.Twitter;
+
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// The factory instance is re-useable and thread safe.
+//	    Twitter twitter = TwitterFactory.getSingleton();
+//	    Status status = twitter.updateStatus(latestStatus);
+//	    System.out.println("Successfully updated the status to [" + status.getText() + "].");
+	}
+
+}
