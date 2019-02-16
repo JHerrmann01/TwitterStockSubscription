7586cdd0eabfde5b57ea8310ae8e5a251ceaa778
unknown <jeremy.herrmann@stonybrook.edu>
2019-02-15 20:36:42 -0500

    Added post sample code

--- src/App.java
+++ src/App.java
@@ -1,9 +1,32 @@
 import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true)
+		  .setOAuthConsumerKey("Aj53Yg5ijyCb0v6KxtunaIxV8")
+		  .setOAuthConsumerSecret("VQb5UMxl2z3tryabee4s8fyh39VpzuY4yN9ThAGs3Uwd4voiha")
+		  .setOAuthAccessToken("907456144297295872-wPrALDxqJiWjpe92JMUGlbft30kNqeH")
+		  .setOAuthAccessTokenSecret("eHqZFhhM13ukT4ttJj7emBl6vE6wDCbZnIY6jRS5IdyCn");
+		TwitterFactory tf = new TwitterFactory(cb.build());
+		Twitter twitter = tf.getInstance();
 		
+		try {
+			String response = createTweet(twitter, "Testing out Twitter API :)");
+			System.out.println(response);
+		} catch (TwitterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	public static String createTweet(Twitter twitter, String tweet) throws TwitterException {
+	    Status status = twitter.updateStatus(tweet);
+	    return status.getText();
 	}
 
 }
